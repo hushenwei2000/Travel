2c45ede18b3e693f24339464c16f74d2c36ed903
hsw2000 <1223929119@qq.com>
2019-12-07 16:51:07 +0800

    add header

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="header">
+      <div class="header-left">&lt;</div>
+      <div class="header-mid">
+          <div class="header-mid-text">奶爸旅游季 游乐园走起</div>
+          
+      </div>
+      <div class="header-right">
+          北京
+        <div class="header-right-arrow"></div>
+        </div>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'HomeHeader'
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/varibles.styl';
+    .header
+        display flex
+        height 48px
+        background-color $bgColor
+        font-size 16px
+        text-align center
+        line-height 48px
+        color white
+        .header-left
+            width: 0.727rem
+            height 48px
+        .header-mid
+            flex 1
+            display flex
+            align-items center
+            .header-mid-text
+                flex 1
+                padding-left .4rem
+                line-height 30px
+                background-color #fff
+                color lightgrey
+                text-align left
+                border-radius .06rem
+        .header-right
+            width: 1.324rem
+            height 48px
+            line-height 48px
+            .header-right-arrow
+                display inline-block
+                vertical-align middle
+                width 0
+                height 0
+                border 6px solid transparent
+                border-top 6px solid #fff
+                border-bottom none
+</style>
